42c72070a95235db98d472549aacf88ac437db96
Carlos Caballero <carlos.caballero.gonzalez@gmail.com>
2020-02-10 16:12:57 +0100

    update angular9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './shared/core.module';
+import { RouterModule } from '@angular/router';
+import { rootRouterConfig } from './app-routing';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    SharedModule,
+    CoreModule,
+    RouterModule.forRoot(rootRouterConfig, { useHash: false })
   ],
+  declarations: [AppComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
